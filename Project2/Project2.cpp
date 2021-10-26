6b3e99864bec7df028fa146a6394f8365bfa55b6
DESKTOP-V1BAHKU\deao3 <deao3558@gmail.com>
2021-10-26 19:30:13 +0900

    2021-10-26 7-29 파이썬 모듈 c에서 데이터 가져올수 있도록 환경조성 팀원들이 스도쿠를 쉽게 만들수있도록 프로젝트 파일 만든후 스도쿠 테이블과 값 지정해놓음

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -1,21 +1,47 @@
-﻿// Project2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿/*
+#include <mysql.h>
 #include <iostream>
+using namespace std;
+int q_estado;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    printf("hello");
+// db 에서 스도쿠 테이블을 꺼내오는 함수 mysql 데이터베이스가 c에 연결되진 않았을떄 사용하지마세요!
+int main() {
+
+	
+	MYSQL* conectar;
+	MYSQL_ROW fila;
+	MYSQL_RES* result;
+	string a;
+	conectar = mysql_init(0);
+	conectar = mysql_real_connect(conectar, "localhost", "root", "1417", "sedoku_table", 3306, NULL, 0);
+	if (conectar) {
+		string consulta = "select * from sedoku";
+		const char* c = consulta.c_str();
+
+		q_estado = mysql_query(conectar, c);
+		if (!q_estado) {
+			result = mysql_store_result(conectar);
+			cout << result << "\n";
+			while (fila = mysql_fetch_row(result)) {
+				cout << fila[2];
+				a = fila[2];
+				getchar();
+				
+
+			}
+			cout << a;
+
+		}
+
+		
+	}
+
+
+	
+	
+
+
+
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+*/
